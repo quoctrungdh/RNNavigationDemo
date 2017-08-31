a59a331543f69e3b81448f7798d7590364aae350
Do Ha Quoc Trung <quoc-trung.do-ha@ekino.com>
2017-08-31 13:52:14 +0700

    feat(mainScreen): adding tabNavigation

--- Components/App.js
+++ Components/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
-import { StackNavigator } from 'react-navigation';
+import { View, Text, Button } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
 import HomeScreen from './HomeScreen';
 import ChatScreen from './ChatScreen';
 
+function RecentChatsScreen({ navigation }) {
+  return (
+    <View>
+      <Text>List of recent chats</Text>
+      <Button
+        onPress={() => navigation.navigate('Chat', { user: 'Lucy' })}
+        title="Chat with Lucy"
+      />
+    </View>
+  )
+}
+
+function AllContactsScreen({ navigation }) {
+  return (
+    <View>
+      <Text>List of all contacts</Text>
+      <Button
+        onPress={() => navigation.navigate('Chat', { user: 'Lucy' })}
+        title="Chat with Lucy"
+      />
+    </View>
+  )
+}
+
+const MainScreenNavigator = TabNavigator({
+  Recent: { screen: RecentChatsScreen },
+  All: { screen: AllContactsScreen }
+})
+
+MainScreenNavigator.navigationOptions = {
+  title: 'My chats'
+}
+
 const App = StackNavigator({
-  Home: { screen: HomeScreen },
+  Home: { screen: MainScreenNavigator },
   Chat: { screen: ChatScreen }
 })
 
