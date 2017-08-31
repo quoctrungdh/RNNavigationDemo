31ac003dde9e2a16abe02387cccb0cca887c153a
Do Ha Quoc Trung <quoc-trung.do-ha@ekino.com>
2017-08-31 13:16:40 +0700

    feat(Home): add Home screen with navigation

---
+++ Components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { StackNavigator } from 'react-navigation';
+
+import HomeScreen from './HomeScreen';
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen }
+})
+
+export default App;
